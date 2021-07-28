019f3e02ce5ee38d002d1a64a9c19669b77c3434
Miles Gardner <milesgardner@uchicago.edu>
2021-07-28 13:27:32 -0400

    DE1 finished

--- miles2kilos.c
+++ miles2kilos.c
@@ -6,8 +6,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* convert_miles_to_km
+* converts miles to kilometers
+* with miles (float) being the parameter
+*/
+
+float convert_miles_to_km(float mil) {
+    float k = mil * 1.609;
+    return k;
+}
+
+/* convert_miles_to_meters
+* converts miles to meters
+* with miles (float) being the parameter
+*/
+
+float convert_miles_to_meters(float mil) {
+    float m = mil * 1609;
+    return m;
+}
+
+
 int main () {
-    //make variables for miles, kilometerss, and meters
+    //make variables for miles, kilometers, and meters
     double miles, kilom, meters;
 
     //print information about program
@@ -18,13 +39,13 @@ int main () {
     scanf("%lf", &miles);
 
     // convert miles to kilometerss
-    kilom = miles * 1.609;
+    kilom = convert_miles_to_km(miles);
+    meters = convert_miles_to_meters(miles);
     
-    //if statement for if kilo > 1, else returns meters
+    //if statement for if kilo > 1, else returns val in meters
     if (kilom > 1) {
         printf("%f miles in kms is %f kilometers! Cool!\n", miles, kilom);
     } else {
-        meters = miles * 1609;
         printf("%f miles in meters is %f meters! Cool! \n", miles, meters);
     }
 
